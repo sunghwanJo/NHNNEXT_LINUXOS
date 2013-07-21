41357f881b2678be82eaed853e9ae8d567b90bc5
sunghwan <sunghwan@sunghwan-VirtualBox.(none)>
2013-07-21 15:54:57 +0900

    *숙제 끝!

---
+++ 20130721/showAllFiles.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+fileList=*
+for file in $fileList
+do
+	sleep 1
+	echo $file
+done
---
+++ 20130721/timer.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+COUNT=$1
+
+#만약 인자값이 없다면 숫자를 받는다
+if [ $# -lt 1 ]; then
+	echo '몇초 입력?'
+	read COUNT
+fi
+
+for i in $(seq 1 $COUNT)
+do	
+	sleep 1
+	echo $i
+done
+echo 
