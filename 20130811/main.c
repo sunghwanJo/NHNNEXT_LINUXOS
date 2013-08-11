792d09421edafbf3d539dd4e8bbac9c6c0b5dc7a
sunghwan <sunghwan@sunghwan-VirtualBox.(none)>
2013-08-11 18:22:54 +0900

    * GOod :D

---
+++ 20130811/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "my.h"
+
+int main()
+{
+	printHello();
+	printf("3 ^ 4 = %5.2f\n", power(3.0, 4));
+	printBye();
+}
---
+++ 20130811/my.h
@@ -0,0 +1,9 @@
+#ifndef MY_H
+#define MY_H
+#include <stdio.h>
+
+double power(double a, int b);
+void printBye();
+void printHello();
+
+#endif
